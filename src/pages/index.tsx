924f367a301171bac327a9d0a3646935559540a3
Lucas Tamir Faria <lucastamir@gmail.com>
2022-11-08 20:28:52 -0300

    feat: criando a primeira query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-
-import SectionHero from 'components/SectionHero'
-import SectionAboutProject from 'components/SectionAboutProject'
-import SectionTech from 'components/SectionTech'
-import SectionConcepts from 'components/SectionConcepts'
-import SectionModules from 'components/SectionModules'
-import SectionAgenda from 'components/SectionAgenda'
-import PricingBox from 'components/PricingBox'
-import SectionAboutUs from 'components/SectionAboutUs'
-import SectionReviews from 'components/SectionReviews'
-import SectionFaq from 'components/SectionFaq'
 import Footer from 'components/Footer'
 import JsonSchema from 'components/JsonSchema'
+import PricingBox from 'components/PricingBox'
+import SectionAboutProject from 'components/SectionAboutProject'
+import SectionAboutUs from 'components/SectionAboutUs'
+import SectionAgenda from 'components/SectionAgenda'
+import SectionConcepts from 'components/SectionConcepts'
+import SectionFaq from 'components/SectionFaq'
+import SectionHero from 'components/SectionHero'
+import SectionModules from 'components/SectionModules'
+import SectionReviews from 'components/SectionReviews'
+import SectionTech from 'components/SectionTech'
+import client from 'graphql/client'
+import GET_LANDING_PAGE from 'graphql/queries/getLandingPage'
+import { GetStaticProps } from 'next'
 
 const Index = () => (
   <>
@@ -29,5 +30,15 @@ const Index = () => (
     <JsonSchema />
   </>
 )
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { landingPage } = await client.request(GET_LANDING_PAGE)
+
+  return {
+    props: {
+      ...landingPage
+    }
+  }
+}
 
 export default Index
